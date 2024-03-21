34361caf6191f55bcabc8dbc129c03155e008fbe
sbgs9 <ganeshsusarla01@gmail.com>
2024-03-20 18:11:00 -0700

    Change userintent method to use zero shot classification

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ data_queue = []
 pdf_queue = []
 work_flag = threading.Event()
 work_flag.set()  # Set the event initially to True
-client = InferenceClient(model='mistralai/Mistral-7B-Instruct-v0.2', token=os.environ.get("HUGGINGFACE_TOKEN"))
+chat_client = InferenceClient(model='mistralai/Mistral-7B-Instruct-v0.2', token=os.environ.get("HUGGINGFACE_TOKEN"))
+intent_client = InferenceClient(model='facebook/bart-large-mnli', token=os.environ.get("HUGGINGFACE_TOKEN"))
 
 def getCredentials():
     api_key = 'AIzaSyCf4Xi9YgFeGfhXS2PGx4f6ymvSFDV6AG0'
@@ -71,7 +72,7 @@ def process_query_thread(query):
 @app.route('/chatresponse', methods=['POST'])
 def chat_response():
     prompt = request.form.get('prompt')
-    data = client.text_generation(max_new_tokens=100, prompt=prompt)
+    data = chat_client.text_generation(max_new_tokens=100, prompt=prompt)
     tokenized_data = tokenize.sent_tokenize(data)
     last_sentence = tokenized_data[-1]
     if last_sentence[-1] == '?' or last_sentence[-1] != '.' or last_sentence[-1] != '!':
@@ -81,26 +82,9 @@ def chat_response():
 
 @app.route('/userintent', methods=['POST'])
 def user_intent():
-    system_prompt = """You are a mental health assistant. 
-    
-    You are supposed to understand the user's query and classify it into one of three possible actions based on what the user wants to do. The three possible actions are the following: chat with a chatbot, search for resources, and book an appointment. 
-    
-    An example for the "chat with a chatbot" option would be if a user asked a question, such as "What is mental health?" or "What is depression?" 
-    
-    An example for the "search for resources" option would be if a user said "can you give me resources for depression?" 
-    
-    An example for the "book an appointment" option would be if a user said "I would like to book an appointment", or "Can I book an appointment?". 
-
-    Based on the user's query, respond with one of those three possible choices in the following format:
-    
-    user_intent:CHAT_WITH_CHATBOT
-    user_intent:SEARCH_FOR_RESOURCES
-    user_intent:BOOK_APPOINTMENT
-    
-    In the above format, respond to the user's query."""
+    labels = ['book appointment', 'view appointments','cancel appointment', 'question', 'chat', 'search for resources']
     prompt = request.form.get('prompt')
-    final_prompt = system_prompt + prompt
-    data = client.text_generation(prompt=final_prompt)
+    data = intent_client.zero_shot_classification(prompt, labels)
     response = jsonify({'user_intent': data})
     return response
 
